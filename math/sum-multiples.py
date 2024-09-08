66314ae6bf4da2248b33b2daa01a1b802a2390dc
boSSmanek <49870808+boSSmanek@users.noreply.github.com>
2024-09-08 23:55:44 +0200

    LC-sum-multiples [Runtime: 56 ms; Memory: 16.5 MB]

--- math/sum-multiples.py
+++ math/sum-multiples.py
@@ -1,11 +1,11 @@
 class Solution:
     def sumOfMultiples(self, n: int) -> int:
-        result = 0
+        #result = 0
 
-        for i in range(1, n + 1):
-            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
-                result += i
+        #for i in range(1, n + 1):
+        #    if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+        #        result += i
 
-        #res = sum([i for i in range(1, n + 1) if i % 3 == 0 or i % 5 == 0 or i % 7 == 0])
+        res = sum([i for i in range(1, n + 1) if i % 3 == 0 or i % 5 == 0 or i % 7 == 0])
 
-        return result
+        return res
