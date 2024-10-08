f3ad31218a1e38b24944a33fb7b04ef4ad95225a
boSSmanek <49870808+boSSmanek@users.noreply.github.com>
2024-10-08 12:25:32 +0200

    LC-number-of-steps-to-reduce-a-number-to-zero [Runtime: 0 ms; Memory: 7.6 MB]

--- bit-manipulation/number-of-steps-to-reduce-a-number-to-zero.cpp
+++ bit-manipulation/number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -4,9 +4,9 @@ public:
         int cnt = 0;
         while (num != 0) {
             if (num % 2 == 0) {
-                num = num / 2;
+                num /= 2;
             } else {
-                num = num - 1;
+                num -= 1;
             }
             cnt++;
         }
